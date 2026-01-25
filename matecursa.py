c41bc4a25f00ecd8eb40e26bf4fd52d2136c6d68
Jordi prats <jordi.prats@gmail.com>
2026-01-25 12:38:27 +0100

    base combinades

--- matecursa.py
+++ matecursa.py
@@ -57,6 +57,57 @@ def sumes(file, pages, min, max, min_segona_unitat, max_segona_unitat, multiplic
       max += 1
   pdf.output(file,'F')
 
+
+#
+# enters (sumes i restes tipus -2 + 12)
+#
+
+@click.command()
+@click.option('--file', default='enters_sumes_restes.pdf', help='output file')
+@click.option('--pages', default=20, help='number of pages')
+@click.option('--min', default=-20, help='min int')
+@click.option('--max', default=20, help='max int')
+@click.option('--disable-total-operacions', is_flag=True, default=True, help='Elimina missatge de total de operacions')
+def enters_sumes_restes(file, pages, min, max, disable_total_operacions):
+  pdf = FPDF(orientation='P', unit='mm', format='A4')
+
+  for pagina in range(0, pages):
+    pdf.add_page()
+    pdf.set_font('helvetica', '', 15.0)
+    pdf.set_xy(5.0, 18)
+    pdf.cell(w=0,h=0, txt='Nom: ........................................................................... Data: ...................................', ln=0 )
+
+    if not disable_total_operacions:
+      pdf.set_xy(5.0, 276)
+      pdf.set_font('helvetica', '', 25.0)
+      pdf.cell(w=0,h=0, txt='Operacions fetes en 2 minuts .......................', ln=0 )
+
+    pdf.set_font('helvetica', '', 15.0)
+
+    anterior_operacio = ''
+    operacio = ''
+
+    for columna in range(0, 4):
+      for linea in range(0,14):
+        pdf.set_xy(10.0 + (columna*50), 32.5 + (linea*17))
+
+        while anterior_operacio == operacio:
+          a = randrange(min, max)
+          b = randrange(min, max)
+
+          operador = random.choice(['+', '-'])
+
+          # format amb parèntesis si és negatiu
+          def fmt(n):
+            return f"({n})" if n < 0 else str(n)
+
+          operacio = f"{fmt(a)} {operador} {fmt(b)} ="
+
+        pdf.cell(w=0,h=0, txt=operacio, ln=0 )
+        anterior_operacio = operacio
+
+  pdf.output(file,'F')
+
 @click.command()
 @click.option('--file', default='sumes_hortizontal.pdf', help='output file')
 @click.option('--pages', default=10, help='number of pages')
@@ -454,6 +505,91 @@ def problemes(file, pagines, sumes_min, sumes_max, mul_min, mul_max):
       pdf.multi_cell(w=0,h=10, txt=str(linea+1)+'. '+problemes_get_pregunta(sumes_min, sumes_max, mul_min, mul_max).encode('latin-1', 'replace').decode('latin-1'))
   pdf.output(file,'F')
 
+#
+# operacions combinades
+#
+
+PATRONS_FACIL = [
+  "(a + b) * c - d",
+  "a + (b - c) * d",
+  "(a - b) + (c * d)",
+  "(a + b) - (c + d)",
+]
+
+PATRONS_MITJA = [
+  "(a * b) - (c - d)",
+  "(a + b) - (c * d)",
+  "a - (b * c) + d",
+  "(a - b) * (c + d)",
+]
+
+PATRONS_DIFICIL = [
+  "(-a + b) * (c - d)",
+  "(a - b) + (-c * d)",
+  "(a + -b) - (c * d)",
+  "(-a + b) - (c + -d)",
+]
+
+def genera_operacio(mode):
+  if mode == "mix":
+    nivell = random.choice(["facil", "mitja", "dificil"])
+  else:
+    nivell = mode
+
+  if nivell == "facil":
+    patro = random.choice(PATRONS_FACIL)
+    nums = {k: randrange(1, 10) for k in ["a","b","c","d"]}
+
+  elif nivell == "mitja":
+    patro = random.choice(PATRONS_MITJA)
+    nums = {k: random.choice([randrange(1,10), -randrange(1,10)]) for k in ["a","b","c","d"]}
+
+  else:  # dificil
+    patro = random.choice(PATRONS_DIFICIL)
+    nums = {k: random.choice([randrange(1,10), -randrange(1,10)]) for k in ["a","b","c","d"]}
+
+  expr = patro
+  for k,v in nums.items():
+    expr = expr.replace(k, str(v))
+
+  return expr.replace("×", "x") + " ="
+
+@click.command()
+@click.option('--file', default='operacions_combinades.pdf')
+@click.option('--pages', default=20)
+@click.option(
+    '--mode',
+    type=click.Choice(['facil', 'mitja', 'dificil', 'mix']),
+    default='mix',
+    help='Nivell de dificultat: facil, mitja, dificil o mix'
+)
+def operacions_combinades(file, pages, mode):
+
+  pdf = FPDF(orientation='P', unit='mm', format='A4')
+
+  for pagina in range(pages):
+    pdf.add_page()
+    pdf.set_font('helvetica', '', 15)
+    pdf.set_xy(5,18)
+    pdf.cell(0,0,'Nom: ........................................ Data: ........................')
+
+    pdf.set_font('helvetica','',20)
+
+    anterior_operacio = ""
+
+    for columna in range(2):
+      for linea in range(14):
+        pdf.set_xy(10 + columna*100, 32.5 + linea*17)
+
+        operacio = ""
+        while operacio == anterior_operacio:
+          operacio = genera_operacio(mode)
+
+        pdf.cell(0,0,operacio)
+        anterior_operacio = operacio
+
+  pdf.output(file,'F')
+
 
 #
 # main
@@ -472,6 +608,9 @@ matecursa.add_command(taules_multiplicar)
 matecursa.add_command(divisions)
 
 matecursa.add_command(problemes)
+
+matecursa.add_command(enters_sumes_restes)
+matecursa.add_command(operacions_combinades)
 
 if __name__ == '__main__':
   matecursa()
